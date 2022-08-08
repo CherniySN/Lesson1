e9396807bb17ff4e10521eb13efb4d05b51f5606
Sergey Nikolaevich Black <cherniyseni@gmail.com>
2022-08-08 17:58:55 +0400

    асчет среднеарифмитического между цифрами которые кратны 3.

--- Kratnoct.py
+++ Kratnoct.py
@@ -7,6 +7,19 @@ class Kratnost:
     def __init__(self, numbers=None):
         if numbers != None:
             self.numbers = list(map(int, numbers))
+        self.totalsum = 0
+        self.i = 0
+        self.average = 0
 
     def rashchet(self):
-        for x in range(self.numbers[0], self.numbers[1]):
+        for x in range(self.numbers[0], self.numbers[1] + 1):
+            if x % 3 == 0:
+                self.totalsum += x
+                self.i += 1
+        self.average = self.totalsum / self.i
+        return (self.average)
+
+
+ob = input('Введите два числа ').split()
+a = Kratnost(ob)
+print(a.rashchet())
