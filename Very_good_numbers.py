c50b15517a5a8c24dfdab107ab63f20000043a1d
Sergey Nikolaevich Black <cherniyseni@gmail.com>
2022-08-08 18:11:49 +0400

    асчет среднеарифмитического между цифрами которые кратны 3.

---
+++ Very_good_numbers.py
@@ -0,0 +1,18 @@
+"""
+Напишите программу, которая находит и выводит все двузначные числа,
+которые равны утроенному произведению своих цифр. К таким относятся, например, 15 и 24.
+"""
+
+
+class NaidemChislo:
+    def __init__(self, chislo=None):
+        if chislo is not None:
+            self.chislo = str(chislo)
+
+    def naidem_chislo(self):
+        if (int(self.chislo[0]) * int(self.chislo[1]) * 3) == int(self.chislo):
+            print('Эт замечательное число.')
+
+
+numbers = NaidemChislo(15)
+numbers.naidem_chislo()
